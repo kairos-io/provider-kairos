ede189146503687a6b8f4fc0386784c88c982868
Ettore Di Giacinto <mudler@users.noreply.github.com>
2022-09-28 19:34:15 +0200

    :robot: Update testing image ref

--- tests/decentralized_test.go
+++ tests/decentralized_test.go
@@ -203,7 +203,7 @@ var _ = Describe("kairos decentralized k8s test", Label("decentralized-k8s"), fu
 		It("upgrades to a specific version", func() {
 			version, _ := machine.SSHCommand("source /etc/os-release; echo $VERSION")
 
-			out, _ := machine.SSHCommand("sudo kairos-agent upgrade --image quay.io/kairos/kairos:opensuse-v1.21.4-32")
+			out, _ := machine.SSHCommand("sudo kairos-agent upgrade --image quay.io/kairos/kairos-opensuse:v1.0.0-rc2-k3sv1.21.14-k3s1")
 			Expect(out).To(ContainSubstring("Upgrade completed"))
 
 			machine.SSHCommand("sudo sync")
