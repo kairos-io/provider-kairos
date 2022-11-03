e973754710a7150b6b278f95e58115856c880c2e
Dimitris Karakasilis <gogs@fake.local>
2022-11-03 18:12:52 +0200

    Add test to check that provider returns a sorted list of versions
    Signed-off-by: Dimitris Karakasilis <dimitris@karakasilis.me>

---
+++ tests/provider_upgrade_test.go
@@ -0,0 +1,47 @@
+// nolint
+package mos_test
+
+import (
+	"encoding/json"
+
+	"github.com/kairos-io/kairos/tests/machine"
+	"github.com/mudler/go-pluggable"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"golang.org/x/mod/semver"
+)
+
+var _ = Describe("provider upgrade test", Label("provider-upgrade"), func() {
+	BeforeEach(func() {
+		machine.EventuallyConnects()
+	})
+
+	AfterEach(func() {
+		if CurrentGinkgoTestDescription().Failed {
+			gatherLogs()
+		}
+	})
+
+	Context("agent.available_releases event", func() {
+		It("returns the available versions ordered", func() {
+			resultStr, _ := machine.SSHCommand(`echo '{ "config": ""}' | /system/providers/agent-provider-kairos agent.available_releases`)
+
+			var result pluggable.EventResponse
+
+			err := json.Unmarshal([]byte(resultStr), &result)
+			Expect(err).ToNot(HaveOccurred())
+
+			Expect(result.Data).ToNot(BeEmpty())
+			var versions []string
+			json.Unmarshal([]byte(result.Data), &versions)
+
+			Expect(versions).ToNot(BeEmpty())
+			sorted := make([]string, len(versions))
+			copy(sorted, versions)
+
+			semver.Sort(sorted)
+
+			Expect(sorted).To(Equal(versions))
+		})
+	})
+})
