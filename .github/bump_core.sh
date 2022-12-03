8f7f20622c013576552f2017e6fc3c8b9bb12718
Ettore Di Giacinto <mudler@users.noreply.github.com>
2022-12-03 16:58:04 +0000

    :robot: Add core image bump pipeline
    Signed-off-by: Ettore Di Giacinto <mudler@users.noreply.github.com>

---
+++ .github/bump_core.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# https://gist.github.com/lukechilds/a83e1d7127b78fef38c2914c4ececc3c
+get_latest_release() {
+  curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
+    grep '"tag_name":' |                                            # Get tag line
+    sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
+}
+
+LATEST=$(get_latest_release "kairos-io/kairos")
+
+echo "$LATEST" > CORE_VERSION
