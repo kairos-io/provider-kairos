ca56b8f4ab7778b14037fcc4c45f6f2ebed155f9
Mauro Morales <mauro.morales@spectrocloud.com>
2024-04-29 12:03:22 +0200

    :sparkles: Refactor edgevpn integration (#549)
    Just a cleanup left from last weeks work
    
    ---------
    
    Signed-off-by: Mauro Morales <mauro.morales@spectrocloud.com>

--- internal/cli/bridge.go
+++ internal/cli/bridge.go
@@ -9,15 +9,12 @@ import (
 	"github.com/ipfs/go-log"
 	qr "github.com/kairos-io/go-nodepair/qrcode"
 	"github.com/kairos-io/kairos-sdk/utils"
-	rcmgr "github.com/libp2p/go-libp2p/p2p/host/resource-manager"
 	"github.com/mudler/edgevpn/api"
 	"github.com/mudler/edgevpn/cmd"
-	"github.com/mudler/edgevpn/pkg/config"
 	"github.com/mudler/edgevpn/pkg/logger"
 	"github.com/mudler/edgevpn/pkg/node"
 	"github.com/mudler/edgevpn/pkg/services"
 	"github.com/mudler/edgevpn/pkg/vpn"
-	"github.com/multiformats/go-multiaddr"
 	"github.com/urfave/cli/v2"
 )
 
@@ -113,90 +110,6 @@ func BridgeCMD(toolName string) *cli.Command {
 	}
 }
 
-func stringsToMultiAddr(peers []string) []multiaddr.Multiaddr {
-	res := []multiaddr.Multiaddr{}
-	for _, p := range peers {
-		addr, err := multiaddr.NewMultiaddr(p)
-		if err != nil {
-			continue
-		}
-		res = append(res, addr)
-	}
-	return res
-}
-
-func configFromContext(c *cli.Context) *config.Config {
-	autorelayInterval, err := time.ParseDuration(c.String("autorelay-discovery-interval"))
-	if err != nil {
-		autorelayInterval = 0
-	}
-	var limitConfig *rcmgr.PartialLimitConfig
-	d := map[string]map[string]interface{}{}
-
-	return &config.Config{
-		NetworkConfig:     c.String("config"),
-		NetworkToken:      c.String("token"),
-		Address:           c.String("address"),
-		Router:            c.String("router"),
-		Interface:         c.String("interface"),
-		Libp2pLogLevel:    c.String("libp2p-log-level"),
-		LogLevel:          c.String("log-level"),
-		LowProfile:        c.Bool("low-profile"),
-		Blacklist:         c.StringSlice("blacklist"),
-		Concurrency:       c.Int("concurrency"),
-		FrameTimeout:      c.String("timeout"),
-		ChannelBufferSize: c.Int("channel-buffer-size"),
-		InterfaceMTU:      c.Int("mtu"),
-		PacketMTU:         c.Int("packet-mtu"),
-		BootstrapIface:    c.Bool("bootstrap-iface"),
-		Whitelist:         stringsToMultiAddr(c.StringSlice("whitelist")),
-		Ledger: config.Ledger{
-			StateDir:         c.String("ledger-state"),
-			AnnounceInterval: time.Duration(c.Int("ledger-announce-interval")) * time.Second,
-			SyncInterval:     time.Duration(c.Int("ledger-syncronization-interval")) * time.Second,
-		},
-		NAT: config.NAT{
-			Service:           c.Bool("natservice"),
-			Map:               c.Bool("natmap"),
-			RateLimit:         c.Bool("nat-ratelimit"),
-			RateLimitGlobal:   c.Int("nat-ratelimit-global"),
-			RateLimitPeer:     c.Int("nat-ratelimit-peer"),
-			RateLimitInterval: time.Duration(c.Int("nat-ratelimit-interval")) * time.Second,
-		},
-		Discovery: config.Discovery{
-			BootstrapPeers: c.StringSlice("discovery-bootstrap-peers"),
-			DHT:            c.Bool("dht"),
-			MDNS:           c.Bool("mdns"),
-			Interval:       time.Duration(c.Int("discovery-interval")) * time.Second,
-		},
-		Connection: config.Connection{
-			AutoRelay:                  c.Bool("autorelay"),
-			MaxConnections:             c.Int("max-connections"),
-			HolePunch:                  c.Bool("holepunch"),
-			StaticRelays:               c.StringSlice("autorelay-static-peer"),
-			AutoRelayDiscoveryInterval: autorelayInterval,
-			OnlyStaticRelays:           c.Bool("autorelay-static-only"),
-			HighWater:                  c.Int("connection-high-water"),
-			LowWater:                   c.Int("connection-low-water"),
-		},
-		Limit: config.ResourceLimit{
-			Enable:      c.Bool("limit-enable"),
-			FileLimit:   c.String("limit-file"),
-			Scope:       c.String("limit-scope"),
-			MaxConns:    c.Int("max-connections"), // Turn to 0 to use other way of limiting. Files take precedence
-			LimitConfig: limitConfig,
-		},
-		PeerGuard: config.PeerGuard{
-			Enable:        c.Bool("peerguard"),
-			PeerGate:      c.Bool("peergate"),
-			Relaxed:       c.Bool("peergate-relaxed"),
-			Autocleanup:   c.Bool("peergate-autoclean"),
-			SyncInterval:  time.Duration(c.Int("peergate-interval")) * time.Second,
-			AuthProviders: d,
-		},
-	}
-}
-
 // bridge is just starting a VPN with edgevpn to the given network token.
 func bridge(c *cli.Context) error {
 	qrCodePath := ""
@@ -234,7 +147,7 @@ func bridge(c *cli.Context) error {
 
 	ctx := context.Background()
 
-	nc := configFromContext(c)
+	nc := cmd.ConfigFromContext(c)
 
 	lvl, err := log.LevelFromString(nc.LogLevel)
 	if err != nil {
