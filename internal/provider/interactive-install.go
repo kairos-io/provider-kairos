87fb463dd704f752fd9e1869b73c32fc3051c51e
Mauro Morales <contact@mauromorales.com>
2025-01-14 13:26:05 +0100

    Revert k0s changes to the interactive installer
    These will be addressed in kairos-io/kairos#3125
    
    Signed-off-by: Mauro Morales <contact@mauromorales.com>

--- internal/provider/interactive-install.go
+++ internal/provider/interactive-install.go
@@ -15,38 +15,15 @@ func InteractiveInstall(e *pluggable.Event) pluggable.EventResponse { //nolint:r
 			YAMLSection: "p2p.network_token",
 			Prompt:      "Insert a network token, leave empty to autogenerate",
 			AskFirst:    true,
-			AskPrompt:   "Do you want to set up a full mesh-support?",
+			AskPrompt:   "Do you want to setup a full mesh-support?",
 			IfEmpty:     node.GenerateNewConnectionData().Base64(),
 		},
 		{
 			YAMLSection: "k3s.enabled",
 			Bool:        true,
-			Prompt:      "Do you want to enable k3s? (Select either k3s or k0s)",
-		},
-		{
-			YAMLSection: "k0s.enabled",
-			Bool:        true,
-			Prompt:      "Do you want to enable k0s? (Select either k3s or k0s)",
+			Prompt:      "Do you want to enable k3s?",
 		},
 	}
-
-	// Check for conflicts between k3s and k0s
-	if k3sEnabled, k0sEnabled := checkK3sK0s(prompts); k3sEnabled && k0sEnabled {
-		// Conflict detected, prompt user to resolve it
-		prompts = append(prompts,
-			bus.YAMLPrompt{
-				YAMLSection: "k3s.enabled",
-				Bool:        false,
-				Prompt:      "Both k3s and k0s are enabled. Do you want to disable k3s?",
-			},
-			bus.YAMLPrompt{
-				YAMLSection: "k0s.enabled",
-				Bool:        false,
-				Prompt:      "Both k3s and k0s are enabled. Do you want to disable k0s?",
-			},
-		)
-	}
-
 	payload, err := json.Marshal(prompts)
 	if err != nil {
 		return ErrorEvent("Failed marshalling JSON input: %s", err.Error())
@@ -57,19 +34,4 @@ func InteractiveInstall(e *pluggable.Event) pluggable.EventResponse { //nolint:r
 		Data:  string(payload),
 		Error: "",
 	}
-}
-
-func checkK3sK0s(prompts []bus.YAMLPrompt) (bool, bool) {
-	var k3sEnabled, k0sEnabled bool
-
-	for _, prompt := range prompts {
-		switch prompt.YAMLSection {
-		case "k3s.enabled":
-			k3sEnabled = prompt.Bool
-		case "k0s.enabled":
-			k0sEnabled = prompt.Bool
-		}
-	}
-
-	return k3sEnabled, k0sEnabled
 }
