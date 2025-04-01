66d62d2406d2f221544eed34288918c0a20a4dce
Mauro Morales <mauro.morales@spectrocloud.com>
2025-04-01 11:47:34 +0200

    Adapt interactive installer to k8s distro (#713)
    fixes https://github.com/kairos-io/kairos/issues/3125
    
    Signed-off-by: Mauro Morales <mauro.morales@spectrocloud.com>

--- internal/provider/interactive-install.go
+++ internal/provider/interactive-install.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 
 	"github.com/kairos-io/kairos-sdk/bus"
+	"github.com/kairos-io/kairos-sdk/utils"
 
 	"github.com/mudler/edgevpn/pkg/node"
 	"github.com/mudler/go-pluggable"
@@ -18,12 +19,23 @@ func InteractiveInstall(e *pluggable.Event) pluggable.EventResponse { //nolint:r
 			AskPrompt:   "Do you want to setup a full mesh-support?",
 			IfEmpty:     node.GenerateNewConnectionData().Base64(),
 		},
-		{
+	}
+
+	// Check which Kubernetes binary is installed
+	if utils.K3sBin() != "" {
+		prompts = append(prompts, bus.YAMLPrompt{
 			YAMLSection: "k3s.enabled",
 			Bool:        true,
 			Prompt:      "Do you want to enable k3s?",
-		},
+		})
+	} else if utils.K0sBin() != "" {
+		prompts = append(prompts, bus.YAMLPrompt{
+			YAMLSection: "k0s.enabled",
+			Bool:        true,
+			Prompt:      "Do you want to enable k0s?",
+		})
 	}
+
 	payload, err := json.Marshal(prompts)
 	if err != nil {
 		return ErrorEvent("Failed marshalling JSON input: %s", err.Error())
