acfd93624bca23f5c9708ce1a5027f622e7116b7
Itxaka <itxaka@kairos.io>
2025-07-23 13:33:19 +0200

    Lint
    Signed-off-by: Itxaka <itxaka@kairos.io>

--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
+	"os"
+
 	"github.com/kairos-io/kairos-sdk/bus"
 	"github.com/kairos-io/provider-kairos/v2/internal/cli"
 	"github.com/kairos-io/provider-kairos/v2/internal/provider"
-	"os"
 )
 
 func checkErr(err error) {
